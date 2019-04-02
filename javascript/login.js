914c6f04da7f188ae3167f58476fe3f7e0e06eb4
JonathanM22 <49132274+JonathanM22@users.noreply.github.com>
2019-04-01 21:08:16 -0400

    Add files via upload

--- javascript/login.js
+++ javascript/login.js
@@ -7,7 +7,7 @@ function validate(){
 
 	if ( username == "demo" && password == "demo123"){
 		alert ("Login successfully");
-		window.location = "success.html"; //redirecting to other page
+		window.location = "profile.html"; //redirecting to other page
 		return false;
 	}
 	else{
